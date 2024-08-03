a32d43b56a5eb9eefc930d63a0fb264047314cda
Mikael Stockman <micke@stockman.se>
2024-08-03 20:49:25 +0200

    Set up a proper test suite (#6)

--- lib/danger_plugin.rb
+++ lib/danger_plugin.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
-require "llm_validator/plugin"
-require "llm_validator/llm_prompter"
 require "llm_validator/builders/hunk_content_builder"
 require "llm_validator/builders/prompt_builder"
+require "llm_validator/file_filter"
+require "llm_validator/llm_prompter"
 require "llm_validator/models/diff_header"
 require "llm_validator/models/file_content"
-require "llm_validator/models/file_filter"
 require "llm_validator/models/llm_response"
+require "llm_validator/plugin"
